e8e62255172a0117e3d8d90cac1c9bf4c529e90f
inot <inotkaa@gmail.com>
2022-02-22 16:54:00 +0300

    - CPU Counter - Network Intefaces

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 [Оригинал](https://www.deviantart.com/iznogud78/art/Conky-Fedora-Tree-795963082 "Оригинал")
 
 ![](https://github.com/inot/conky_mint_tree/blob/main/mint_tree.png?raw=true)
+
+# Установка 
+```bash
+mkdir ~/conky
+git clone https://github.com/inot/conky_mint_tree.git
+/usr/bin/conky -c /home/inot/conky/conky_mint_tree/.conkyrc
+```
+
---
+++ cpu_network.lua
@@ -0,0 +1,108 @@
+function Split(s, delimiter)
+    result = {};
+    for match in (s .. delimiter):gmatch("(.-)" .. delimiter) do
+        table.insert(result, match);
+    end
+    return result;
+end
+
+function check_network_connected(routes_text, interface)
+    if string.find(routes_text, interface) then
+        return true
+    end
+    return false
+end
+
+function conky_mynetwork()
+    local file = io.popen("ls -1 /sys/class/net")
+    local interfaces = file:read("*a")
+    file:close()
+
+    local file = io.popen("cat /proc/net/route")
+    local routes_text = file:read("*a")
+    file:close()
+
+    interfaces_a = Split(interfaces, "\n")
+
+    for num, inter in pairs(interfaces_a) do
+        if (inter == "" or inter == "lo") then
+            table.remove(interfaces_a, num)
+        end
+    end
+
+    result = ""
+    if (#interfaces_a > 0) then
+        if (#interfaces_a > 1) then
+            for i = 1, #interfaces_a do
+                if (check_network_connected(routes_text, interfaces_a[i])) then
+                    if (i ~= #interfaces_a) then
+                        result = result ..
+                                     "${alignr}${offset -32}                                                           │\n"
+                        result = result .. "${alignr}${offset -32}[ ${addr " .. interfaces_a[i] .. "} ] " ..
+                                     interfaces_a[i] .. " ─┤\n"
+                        result = result .. "${alignr}${offset -32}[ ${downspeed " .. interfaces_a[i] ..
+                                     "} k/s ] download ─┤    │\n"
+                        result = result .. "${alignr}${offset -32}[ ${downspeedgraph " .. interfaces_a[i] ..
+                                     " 12,120 136311 11B014} ] ─┘    │    │\n"
+                        result = result .. "${alignr}${offset -32}[ ${upspeed " .. interfaces_a[i] ..
+                                     "} k/s ]   upload ─┘    │\n"
+                        result = result .. "${alignr}${offset -32}[ ${upspeedgraph " .. interfaces_a[i] ..
+                                     " 12,120 BA0B0B FC0707} ] ─┘         │\n"
+
+                    else
+                        result = result ..
+                                     "${alignr}${offset -32}                                                           │\n"
+                        result = result .. "${alignr}${offset -32}[ ${addr " .. interfaces_a[i] .. "} ] " ..
+                                     interfaces_a[i] .. " ─┘\n"
+                        result = result .. "${alignr}${offset -32}[ ${downspeed " .. interfaces_a[i] ..
+                                     "} k/s ] download ─┤     \n"
+                        result = result .. "${alignr}${offset -32}[ ${downspeedgraph " .. interfaces_a[i] ..
+                                     " 12,120 136311 11B014} ] ─┘    │     \n"
+                        result = result .. "${alignr}${offset -32}[ ${upspeed " .. interfaces_a[i] ..
+                                     "} k/s ]   upload ─┘     \n"
+                        result = result .. "${alignr}${offset -62}[ ${upspeedgraph " .. interfaces_a[i] ..
+                                     " 12,120 BA0B0B FC0707} ] ─┘     \n"
+                    end
+                end
+            end
+        else
+            if (check_network_connected(routes_text, interfaces_a[1])) then
+                result = result ..
+                             "${alignr}${offset -32}                                                           │\n"
+                result = result .. "${alignr}${offset -32}[ ${addr " .. interfaces_a[1] .. "} ] " .. interfaces_a[1] ..
+                             " ─┘\n"
+                result = result .. "${alignr}${offset -32}[ ${downspeed " .. interfaces_a[1] ..
+                             "} k/s ] download ─┤     \n"
+                result = result .. "${alignr}${offset -32}[ ${downspeedgraph " .. interfaces_a[1] ..
+                             " 12,120 136311 11B014} ] ─┘    │     \n"
+                result = result .. "${alignr}${offset -32}[ ${upspeed " .. interfaces_a[1] ..
+                             "} k/s ]   upload ─┘     \n"
+                result = result .. "${alignr}${offset -62}[ ${upspeedgraph " .. interfaces_a[1] ..
+                             " 12,120 BA0B0B FC0707} ] ─┘     \n"
+            else
+                result = "${alignr}${offset -32} No Network Available ─┘"
+            end
+        end
+    else
+        result = "${alignr}${offset -32} No Network Available ─┘"
+    end
+    return result
+end
+
+function conky_mycpus()
+    local file = io.popen("grep -c processor /proc/cpuinfo")
+    local numcpus = file:read("*n")
+    file:close()
+    listcpus = ""
+    for i = 1, numcpus do
+        if (i == numcpus) then
+            listcpus = listcpus .. "${alignr}${offset -6}[ ${cpu cpu" .. tostring(i) .. "}% ] cpu" .. tostring(i) ..
+                           " ─┘   │   │   │"
+        else
+            listcpus = listcpus .. "${alignr}${offset -6}[ ${cpu cpu" .. tostring(i) .. "}% ] cpu" .. tostring(i) ..
+                           " ─┤   │   │   │\n"
+        end
+    end
+    return listcpus
+end
+
