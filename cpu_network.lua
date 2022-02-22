13a5b6e65afe32e8c1112c48a8cf83a4c16389ae
inot <inotkaa@gmail.com>
2022-02-22 17:37:19 +0300

    - Small Changes

--- cpu_network.lua
+++ cpu_network.lua
@@ -29,62 +29,64 @@ function conky_mynetwork()
             table.remove(interfaces_a, num)
         end
     end
-
+    offset_int = 56
     result = ""
     if (#interfaces_a > 0) then
         if (#interfaces_a > 1) then
             for i = 1, #interfaces_a do
                 if (check_network_connected(routes_text, interfaces_a[i])) then
                     if (i ~= #interfaces_a) then
-                        result = result ..
-                                     "${alignr}${offset -32}                                                           │\n"
-                        result = result .. "${alignr}${offset -32}[ ${addr " .. interfaces_a[i] .. "} ] " ..
-                                     interfaces_a[i] .. " ─┤\n"
-                        result = result .. "${alignr}${offset -32}[ ${downspeed " .. interfaces_a[i] ..
-                                     "} k/s ] download ─┤    │\n"
-                        result = result .. "${alignr}${offset -32}[ ${downspeedgraph " .. interfaces_a[i] ..
-                                     " 12,120 136311 11B014} ] ─┘    │    │\n"
-                        result = result .. "${alignr}${offset -32}[ ${upspeed " .. interfaces_a[i] ..
+                        result = result .. "${alignr}${offset -" .. offset_int ..
+                                     "}                                                           │\n"
+                        result = result .. "${alignr}${offset -" .. offset_int .. "}[ ${addr " .. interfaces_a[i] ..
+                                     "} ] " .. interfaces_a[i] .. " ─┤\n"
+                        result =
+                            result .. "${alignr}${offset -" .. offset_int .. "}[ ${downspeed " .. interfaces_a[i] ..
+                                "} k/s ] download ─┤    │\n"
+                        result = result .. "${alignr}${offset -" .. offset_int .. "}[ ${downspeedgraph " ..
+                                     interfaces_a[i] .. " 12,120 136311 11B014} ] ─┘    │    │\n"
+                        result = result .. "${alignr}${offset -" .. offset_int .. "}[ ${upspeed " .. interfaces_a[i] ..
                                      "} k/s ]   upload ─┘    │\n"
-                        result = result .. "${alignr}${offset -32}[ ${upspeedgraph " .. interfaces_a[i] ..
-                                     " 12,120 BA0B0B FC0707} ] ─┘         │\n"
+                        result = result .. "${alignr}${offset -" .. offset_int .. "}[ ${upspeedgraph " ..
+                                     interfaces_a[i] .. " 12,120 BA0B0B FC0707} ] ─┘         │\n"
 
                     else
-                        result = result ..
-                                     "${alignr}${offset -32}                                                           │\n"
-                        result = result .. "${alignr}${offset -32}[ ${addr " .. interfaces_a[i] .. "} ] " ..
-                                     interfaces_a[i] .. " ─┘\n"
-                        result = result .. "${alignr}${offset -32}[ ${downspeed " .. interfaces_a[i] ..
-                                     "} k/s ] download ─┤     \n"
-                        result = result .. "${alignr}${offset -32}[ ${downspeedgraph " .. interfaces_a[i] ..
-                                     " 12,120 136311 11B014} ] ─┘    │     \n"
-                        result = result .. "${alignr}${offset -32}[ ${upspeed " .. interfaces_a[i] ..
+                        result = result .. "${alignr}${offset -" .. offset_int ..
+                                     "}                                                           │\n"
+                        result = result .. "${alignr}${offset -" .. offset_int .. "}[ ${addr " .. interfaces_a[i] ..
+                                     "} ] " .. interfaces_a[i] .. " ─┘\n"
+                        result =
+                            result .. "${alignr}${offset -" .. offset_int .. "}[ ${downspeed " .. interfaces_a[i] ..
+                                "} k/s ] download ─┤     \n"
+                        result = result .. "${alignr}${offset -" .. offset_int .. "}[ ${downspeedgraph " ..
+                                     interfaces_a[i] .. " 12,120 136311 11B014} ] ─┘    │     \n"
+                        result = result .. "${alignr}${offset -" .. offset_int .. "}[ ${upspeed " .. interfaces_a[i] ..
                                      "} k/s ]   upload ─┘     \n"
-                        result = result .. "${alignr}${offset -62}[ ${upspeedgraph " .. interfaces_a[i] ..
-                                     " 12,120 BA0B0B FC0707} ] ─┘     \n"
+                        result = result .. "${alignr}${offset -" .. offset_int .. "}[ ${upspeedgraph " ..
+                                     interfaces_a[i] .. " 12,120 BA0B0B FC0707} ] ─┘          \n"
                     end
                 end
             end
         else
             if (check_network_connected(routes_text, interfaces_a[1])) then
-                result = result ..
-                             "${alignr}${offset -32}                                                           │\n"
-                result = result .. "${alignr}${offset -32}[ ${addr " .. interfaces_a[1] .. "} ] " .. interfaces_a[1] ..
-                             " ─┘\n"
-                result = result .. "${alignr}${offset -32}[ ${downspeed " .. interfaces_a[1] ..
+                result = result .. "${alignr}${offset -" .. offset_int ..
+                             "}                                                           │\n"
+                result = result .. "${alignr}${offset -" .. offset_int .. "}[ ${addr " .. interfaces_a[1] .. "} ] " ..
+                             interfaces_a[1] .. " ─┘\n"
+                result = result .. "${alignr}${offset -" .. offset_int .. "}[ ${downspeed " .. interfaces_a[1] ..
                              "} k/s ] download ─┤     \n"
-                result = result .. "${alignr}${offset -32}[ ${downspeedgraph " .. interfaces_a[1] ..
+                result = result .. "${alignr}${offset -" .. offset_int .. "}[ ${downspeedgraph " .. interfaces_a[1] ..
                              " 12,120 136311 11B014} ] ─┘    │     \n"
-                result = result .. "${alignr}${offset -32}[ ${upspeed " .. interfaces_a[1] ..
+                result = result .. "${alignr}${offset -" .. offset_int .. "}[ ${upspeed " .. interfaces_a[1] ..
                              "} k/s ]   upload ─┘     \n"
-                result = result .. "${alignr}${offset -62}[ ${upspeedgraph " .. interfaces_a[1] ..
-                             " 12,120 BA0B0B FC0707} ] ─┘     \n"
+                result = result .. "${alignr}${offset -" .. offset_int .. "}[ ${upspeedgraph " .. interfaces_a[1] ..
+                             " 12,120 BA0B0B FC0707} ] ─┘          \n"
             else
-                result = "${alignr}${offset -32} No Network Available ─┘"
+                result = "${alignr}${offset -" .. offset_int .. "} No Network Available ─┘"
             end
         end
     else
-        result = "${alignr}${offset -32} No Network Available ─┘"
+        result = "${alignr}${offset -" .. offset_int .. "} No Network Available ─┘"
     end
     return result
 end
@@ -97,12 +99,11 @@ function conky_mycpus()
     for i = 1, numcpus do
         if (i == numcpus) then
             listcpus = listcpus .. "${alignr}${offset -6}[ ${cpu cpu" .. tostring(i) .. "}% ] cpu" .. tostring(i) ..
-                           " ─┘   │   │   │"
+                           " ─┘   │   │    "
         else
             listcpus = listcpus .. "${alignr}${offset -6}[ ${cpu cpu" .. tostring(i) .. "}% ] cpu" .. tostring(i) ..
-                           " ─┤   │   │   │\n"
+                           " ─┤   │   │    \n"
         end
     end
     return listcpus
 end
-
